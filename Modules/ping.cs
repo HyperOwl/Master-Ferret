82b59fb2c5ec863f1561daa78fa872ca8894a818
HyperOwl <dduo2011@gmail.com>
2018-07-02 17:55:11 -0700

    Added ping command

---
+++ Modules/ping.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master_Ferret.Modules
+{
+    public class ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task pingAsync()
+        {
+            await ReplyAsync("pong");
+        }
+    }
+}
