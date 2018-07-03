9af71a9d1d1dac7fb68c0503744f0c4ae1678353
HyperOwl <dduo2011@gmail.com>
2018-07-02 20:30:03 -0700

    Added RNG

---
+++ Modules/rng.cs
@@ -0,0 +1,34 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master_Ferret.Modules
+{
+    public class rng: ModuleBase<SocketCommandContext>
+    {
+        public async Task rngAsync(int one,int two)
+        {
+            Random gen = new Random();
+            int result = gen.Next(GetMin(one,two), GetMax(one,two));
+            if (one == two)
+            {
+                await Context.Channel.SendMessageAsync("The two numbers are equal.");
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync(result.ToString());
+            }
+        }
+        public static int GetMax(int first, int second)
+        {
+            return first > second ? first : second;
+        }
+        public static int GetMin(int first, int second)
+        {
+            return first < second ? first : second;
+        }
+    }
+}
