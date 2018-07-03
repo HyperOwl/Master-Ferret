89e9dac7675b1951b052ddebb9e6e3b1c8f79e60
HyperOwl <dduo2011@gmail.com>
2018-07-03 11:12:05 -0700

    Added embed to RNG

--- Modules/rng.cs
+++ Modules/rng.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,17 +10,23 @@ namespace Master_Ferret.Modules
 {
     public class rng: ModuleBase<SocketCommandContext>
     {
-        public async Task rngAsync(int one,int two)
+        private static readonly Random gen = new Random();
+        private static readonly Random color = new Random();
+        [Command("rng")]
+        public async Task rngAsync(int one, int two)
         {
-            Random gen = new Random();
-            int result = gen.Next(GetMin(one,two), GetMax(one,two));
+            int result = gen.Next(GetMin(one, two), GetMax(one, two));
             if (one == two)
             {
                 await Context.Channel.SendMessageAsync("The two numbers are equal.");
             }
             else
             {
-                await Context.Channel.SendMessageAsync(result.ToString());
+                var embed = new EmbedBuilder();
+                embed.WithTitle(Context.User.Username + " rolled between " + one + " " + two);
+                embed.WithDescription("The result is " + result.ToString());
+                embed.WithColor(new Color(color.Next(0, 255), color.Next(0, 255), color.Next(0, 255)));
+                await Context.Channel.SendMessageAsync("", false, embed);
             }
         }
         public static int GetMax(int first, int second)
